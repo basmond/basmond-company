76ac0dfe49bb283478c057063f6d8c256b915a3a
basmond <tyrnysuper@gmail.com>
2026-07-08 21:35:33 +0300

    Update album.js

--- js/album.js
+++ js/album.js
@@ -1,43 +1,123 @@
 const tracksContainer = document.getElementById("tracks");
 
+
 tracks.forEach((track,index)=>{
 
-    tracksContainer.innerHTML+=`
 
-    <div class="track" data-index="${index}">
+tracksContainer.innerHTML += `
 
-        <div class="track-number">
-        
-            <span class="number">
+
+<div class="track" data-index="${index}">
+
+
+    <div class="track-number">
+
+        <span class="number">
             ${index+1}
-            </span>
-            
-            <span class="play-icon">
+        </span>
+
+        <span class="play-icon">
             ▶
-            </span>
-            
-        </div>
-
-        <div class="track-title">
-            ${track.title}
-        </div>
-
-        <div class="track-duration">
-            ${track.duration}
-        </div>
+        </span>
 
     </div>
 
-    `;
+
+
+    <div class="track-title">
+
+        ${track.title}
+
+    </div>
+
+
+
+    <div class="track-plays">
+
+        ${track.plays}
+
+    </div>
+
+
+
+    <button class="lyrics-btn" data-lyrics="${index}">
+
+        Lyrics
+
+    </button>
+
+
+
+    <div class="track-duration">
+
+        ${track.duration}
+
+    </div>
+
+
+</div>
+
+
+`;
+
 
 });
+
+
+
+
+
+// запуск трека
 
 document.querySelectorAll(".track").forEach(track=>{
 
-    track.addEventListener("click",()=>{
 
-        playTrack(Number(track.dataset.index));
+track.addEventListener("click",(e)=>{
 
-    });
+
+if(e.target.classList.contains("lyrics-btn"))
+return;
+
+
+playTrack(Number(track.dataset.index));
+
 
 });
+
+
+});
+
+
+
+
+// открытие текста
+
+document.querySelectorAll(".lyrics-btn").forEach(button=>{
+
+
+button.addEventListener("click",()=>{
+
+
+const index = button.dataset.lyrics;
+
+
+openLyrics(index);
+
+
+});
+
+
+});
+
+
+
+
+// окно текста
+
+function openLyrics(index){
+
+
+alert(tracks[index].lyrics);
+
+
+}
