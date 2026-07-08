f77ef004646cde59a986a94f8774c047e94558cd
basmond <tyrnysuper@gmail.com>
2026-07-08 21:36:22 +0300

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,119 +1,144 @@
 const audio = document.getElementById("audio");
 
+
 let currentTrack = 0;
+
 
 const player = document.getElementById("player");
 
+
+
 function playTrack(index){
 
-    currentTrack=index;
 
-    audio.src=tracks[index].file;
+currentTrack=index;
 
-    audio.play();
 
-    player.classList.add("show");
+audio.src=tracks[index].file;
 
-    document.getElementById("playerTitle").textContent=
-    tracks[index].title;
 
-    document.getElementById("durationTime").textContent=
-    tracks[index].duration;
 
-    document.getElementById("playPauseBtn").textContent="⏸";
+player.classList.add("show");
 
-    updateActiveTrack();
+
+
+document.getElementById("playerTitle").textContent =
+tracks[index].title;
+
+
+
+document.getElementById("playPauseBtn").textContent="⏸";
+
+
+
+audio.play().catch(()=>{});
+
+
+
+updateActiveTrack();
+
 
 }
+
+
 
 function updateActiveTrack(){
 
-    document.querySelectorAll(".track").forEach(track=>{
 
-        track.classList.remove("active");
+document.querySelectorAll(".track").forEach(track=>{
 
-    });
-
-    document
-    .querySelector(`[data-index="${currentTrack}"]`)
-    .classList.add("active");
-
-}
-
-function formatTime(time){
-
-    const m=Math.floor(time/60);
-
-    const s=Math.floor(time%60);
-
-    return `${m}:${s<10?"0":""}${s}`;
-
-}
-
-audio.addEventListener("timeupdate",()=>{
-
-    const percent=(audio.currentTime/audio.duration)*100;
-
-    progressBar.value=percent||0;
-
-    currentTime.textContent=formatTime(audio.currentTime);
+track.classList.remove("active");
 
 });
 
-progressBar.addEventListener("input",()=>{
 
-    audio.currentTime=(progressBar.value/100)*audio.duration;
 
-});
+document
+.querySelector(`[data-index="${currentTrack}"]`)
+.classList.add("active");
+
+
+}
+
+
 
 playPauseBtn.onclick=()=>{
 
-    if(audio.paused){
 
-        audio.play();
+if(audio.paused){
 
-        playPauseBtn.textContent="⏸";
+audio.play();
 
-    }else{
+playPauseBtn.textContent="⏸";
 
-        audio.pause();
 
-        playPauseBtn.textContent="▶";
+}else{
 
-    }
+
+audio.pause();
+
+playPauseBtn.textContent="▶";
+
+
+}
+
 
 };
+
+
+
 
 nextBtn.onclick=()=>{
 
-    currentTrack++;
 
-    if(currentTrack>=tracks.length)
-        currentTrack=0;
+currentTrack++;
 
-    playTrack(currentTrack);
+
+if(currentTrack>=tracks.length)
+
+currentTrack=0;
+
+
+
+playTrack(currentTrack);
+
 
 };
+
+
+
 
 prevBtn.onclick=()=>{
 
-    currentTrack--;
 
-    if(currentTrack<0)
-        currentTrack=tracks.length-1;
+currentTrack--;
 
-    playTrack(currentTrack);
+
+if(currentTrack<0)
+
+currentTrack=tracks.length-1;
+
+
+
+playTrack(currentTrack);
+
 
 };
+
+
+
 
 audio.onended=()=>{
 
-    nextBtn.click();
+nextBtn.click();
 
 };
 
+
+
+
 albumPlay.onclick=()=>{
 
-    playTrack(0);
+playTrack(0);
 
 };
