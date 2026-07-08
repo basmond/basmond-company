d22ee9bdc306b82d12a49641ac03fc4b099d5838
basmond <tyrnysuper@gmail.com>
2026-07-08 21:11:00 +0300

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,1 +1,49 @@
+const audio = document.getElementById("audio");
 
+let currentTrack = 0;
+
+function playTrack(index){
+
+    currentTrack = index;
+
+    audio.src = tracks[index].file;
+
+    audio.play();
+
+    updateActiveTrack();
+
+}
+
+function pauseTrack(){
+
+    audio.pause();
+
+}
+
+function toggleTrack(){
+
+    if(audio.paused){
+
+        audio.play();
+
+    }else{
+
+        audio.pause();
+
+    }
+
+}
+
+function updateActiveTrack(){
+
+    document.querySelectorAll(".track").forEach(track=>{
+
+        track.classList.remove("active");
+
+    });
+
+    document
+        .querySelector(`[data-index="${currentTrack}"]`)
+        .classList.add("active");
+
+}
