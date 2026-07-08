bd2326ddc465f28c638c2100b4989feffef972ce
basmond <tyrnysuper@gmail.com>
2026-07-08 21:05:05 +0300

    Update tracks.js

--- js/tracks.js
+++ js/tracks.js
@@ -1,1 +1,27 @@
-
+const tracks = [
+    {
+        title: "Intro",
+        duration: "0:57",
+        file: "assets/audio/intro.mp3"
+    },
+    {
+        title: "Two Six",
+        duration: "3:16",
+        file: "assets/audio/twosix.mp3"
+    },
+    {
+        title: "SAFETY",
+        duration: "5:18",
+        file: "assets/audio/safety.mp3"
+    },
+    {
+        title: "Run A Train (with Future)",
+        duration: "4:02",
+        file: "assets/audio/runatrain.mp3"
+    },
+    {
+        title: "Poor Thang",
+        duration: "4:50",
+        file: "assets/audio/poorthang.mp3"
+    }
+];
