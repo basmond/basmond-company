6eab269a91a1d461bc6e5a9cc287932feaab4b56
basmond <tyrnysuper@gmail.com>
2026-07-10 21:26:59 +0300

    Update tracks.js

--- js/tracks.js
+++ js/tracks.js
@@ -1,23 +1,41 @@
 const albumTracks = [
 
     {
-        disk: "Диск 1",
+        disk: "Глава 1",
         tracks: [
 
             {
-                title:"Night Drive",
-                plays:"18 532",
-                duration:"3:24",
-                file:"assets/audio/night-drive.mp3",
+                title:"-",
+                plays:"0",
+                duration:"0",
+                file:"assets/audio/-.mp3",
                 lyrics:"Текст первой песни..."
             },
 
 
             {
-                title:"Broken Glass",
-                plays:"14 821",
-                duration:"2:58",
-                file:"assets/audio/broken-glass.mp3",
+                title:"Fresh Bloom (w/ Lillith)",
+                plays:"0",
+                duration:"0",
+                file:"assets/audio/freshbloom.mp3",
+                lyrics:"Текст второй песни..."
+            },
+
+            
+            {
+                title:"Eight",
+                plays:"0",
+                duration:"0",
+                file:"assets/audio/eight.mp3",
+                lyrics:"Текст второй песни..."
+            },
+
+            
+            {
+                title:"Outro (I'm Retired)",
+                plays:"0",
+                duration:"0",
+                file:"assets/audio/outro.mp3",
                 lyrics:"Текст второй песни..."
             }
 
@@ -27,16 +45,108 @@ const albumTracks = [
 
 
     {
-        disk:"Диск 2",
+        disk:"Глава 2",
 
         tracks:[
 
             {
-                title:"Dreams",
-                plays:"8 011",
-                duration:"4:18",
-                file:"assets/audio/dreams.mp3",
+                title:"Assemble A Suit (Interlude)",
+                plays:"0",
+                duration:"0",
+                file:"assets/audio/interlude1.mp3",
                 lyrics:"Текст третьей песни..."
+            },
+
+
+            {
+                title:"Tha Wayne Comedy (w/ Lil Wayne)",
+                plays:"0",
+                duration:"0",
+                file:"assets/audio/thawaynecomedy.mp3",
+                lyrics:"Текст второй песни..."
+            },
+
+            
+            {
+                title:"2Casus Rolli (w/ 2SMILEY & Yung Rollin)",
+                plays:"0",
+                duration:"0",
+                file:"assets/audio/2casusrolli.mp3",
+                lyrics:"Текст второй песни..."
+            },
+
+            
+            {
+                title:"Sleep Sheep (Interlude)",
+                plays:"0",
+                duration:"0",
+                file:"assets/audio/interlude2.mp3",
+                lyrics:"Текст второй песни..."
+            }
+
+        ]
+
+    },
+
+
+
+    {
+        disk:"Глава 3",
+
+        tracks:[
+
+            {
+                title:"- (w/ Anderson .Paak)",
+                plays:"0",
+                duration:"0",
+                file:"assets/audio/-.mp3",
+                lyrics:"Текст третьей песни..."
+            },
+
+
+            {
+                title:"Truestory",
+                plays:"0",
+                duration:"0",
+                file:"assets/audio/truestory.mp3",
+                lyrics:"Текст второй песни..."
+            },
+
+            
+            {
+                title:"Skit",
+                plays:"0",
+                duration:"0",
+                file:"assets/audio/skit.mp3",
+                lyrics:"Текст второй песни..."
+            }
+
+        ]
+
+    },
+
+
+
+    {
+        disk:"Глава 4",
+
+        tracks:[
+
+            {
+                title:"Who Is Shawn Wayne?",
+                plays:"0",
+                duration:"0",
+                file:"assets/audio/whoisshawnwayne.mp3",
+                lyrics:"Текст третьей песни..."
+            },
+
+
+            {
+                title:"Late Night In LS 2.0",
+                plays:"0",
+                duration:"0",
+                file:"assets/audio/lnis2.mp3",
+                lyrics:"Текст второй песни..."
             }
 
         ]
